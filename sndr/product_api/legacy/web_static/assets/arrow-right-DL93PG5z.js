a8be9a6784b7680e696f033dad732e96c554faed
Sandermage <sander.odessa@gmail.com>
2026-06-09 04:12:32 +0300

    chore(gui): rebuild served web_static bundle

---
+++ sndr/product_api/legacy/web_static/assets/arrow-right-DL93PG5z.js
@@ -0,0 +1,1 @@
+import{ln as e}from"./index-Q7_6-iu9.js";var t=e(`ArrowRight`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`m12 5 7 7-7 7`,key:`xquz4c`}]]);export{t};
---
+++ sndr/product_api/legacy/web_static/assets/arrow-up-Vk4j6yof.js
@@ -0,0 +1,1 @@
+import{ln as e}from"./index-Q7_6-iu9.js";var t=e(`ArrowUp`,[[`path`,{d:`m5 12 7-7 7 7`,key:`hav0vg`}],[`path`,{d:`M12 19V5`,key:`x0mq9r`}]]);export{t};
